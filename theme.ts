2e5bf89510a2648c79a9e3e51b4be256bc033efb
Darryl Brooks <darrylbrooks13@gmail.com>
2022-04-24 20:12:50 -0500

    feat: initial commit

---
+++ theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const config: ThemeConfig = {
+	useSystemColorMode: false,
+};
+
+const breakpoints = createBreakpoints({
+	sm: '320px',
+	md: '768px',
+	lg: '920px',
+	xl: '1200px',
+});
+
+export const theme = extendTheme({
+	config,
+	fonts: {
+		body: 'Inter, sans-serif',
+	},
+	breakpoints,
+});
