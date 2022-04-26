c6016a521e4ceeb354be8417e69a3414ecf73e70
Darryl Brooks <darrylbrooks13@gmail.com>
2022-04-26 18:28:49 -0500

    feat: adding next auth init files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { theme } from '../theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
 		</ChakraProvider>
 	);
 }
