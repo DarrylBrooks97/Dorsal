84685231640b72e79d3e54c39b1ee9bd96a27647
Darryl Brooks <darrylbrooks13@gmail.com>
2022-04-29 00:22:19 -0500

    refactor: updating prisma client for production env

--- clients/prisma/index.ts
+++ clients/prisma/index.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient({
-	log: ['error', 'warn'],
+	log:
+		process.env.NODE_ENV === 'development'
+			? ['error', 'warn', 'info']
+			: ['error'],
 });
