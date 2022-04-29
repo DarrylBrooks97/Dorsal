ffd91e2f4f9c7132898917b27cd9f8cee7fd57f2
Darryl Brooks <darrylbrooks13@gmail.com>
2022-04-28 19:30:24 -0500

    feat: smoother card scrolling

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,54 @@
-import Card from '@components/Card';
+// import Card from '@components/Card';
 import AddButton from '@components/AddButton';
 import { Tank } from '@prisma/client';
 import { userHooks } from 'hooks/userHooks';
 import { Box, Text, HStack, Stack, Center, Heading } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { headerOptions } from '@/constants';
 import Image from 'next/image';
+import handleViewport from 'react-in-viewport';
+
+const TempComponent = (props: { inViewport: boolean; forwardedRef: any }) => {
+	const { inViewport, forwardedRef } = props;
+	return (
+		<Center
+			w="calc(100vw - 3rem)"
+			h="50vh"
+			bg={inViewport ? 'red' : 'white'}
+			transition="all 4s"
+			scrollSnapAlign="start"
+			ref={forwardedRef}
+		>
+			Hello
+		</Center>
+	);
+};
+
+const ViewportBlock = handleViewport(TempComponent, {
+	threshold: 1,
+});
+
+const componentCards: JSX.Element[] = [
+	ViewportBlock,
+	ViewportBlock,
+	ViewportBlock,
+	ViewportBlock,
+];
 
 export default function Home() {
 	// const { getUser } = userHooks();
+	const cardRef = useRef<HTMLDivElement>(null);
 	const [pos, setPos] = useState(0);
+	const [navClicked, setNavClicked] = useState(false);
 	// const aquariums = getUser<Tank[]>('/api/user/aquariums');
+
+	useEffect(() => {
+		cardRef?.current?.scrollTo({
+			// @ts-ignore
+			left: cardRef.current.children[pos].offsetLeft,
+			behavior: 'smooth',
+		});
+	}, [pos]);
 
 	return (
 		<Box w="full" h="full" p="6" bg="black">
@@ -31,8 +69,11 @@ export default function Home() {
 						<Text
 							fontSize="xl"
 							color={pos === idx ? 'white' : 'gray.500'}
-							transition=".2s ease-in-out"
-							onClick={() => setPos(idx)}
+							transition=".3s ease-in-out"
+							onClick={() => {
+								setNavClicked(true);
+								setPos(idx);
+							}}
 						>
 							{option.name}
 						</Text>
@@ -54,8 +95,27 @@ export default function Home() {
 					))}
 				</HStack>
 			</Stack>
-			<HStack shouldWrapChildren w="full" h="full">
-				<Stack
+			<Center bg="white" w="full" h="full">
+				<HStack
+					ref={cardRef}
+					overflow="scroll"
+					spacing={2}
+					alignItems="start"
+					overflowX="scroll"
+					scrollSnapType="x mandatory"
+					shouldWrapChildren
+				>
+					{componentCards.map((Card, idx) => (
+						// @ts-ignore
+						<Card
+							onEnterViewport={() => {
+								console.log({ navClicked });
+								if (pos === idx) setNavClicked(false);
+								setPos(navClicked ? pos : idx);
+							}}
+						/>
+					))}
+					{/* <Stack
 					justify="center"
 					align="center"
 					borderRadius="15"
@@ -73,8 +133,9 @@ export default function Home() {
 						blurDataURL="https://images.unsplash.com/photo-1512391806023-e43a4e65899f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
 					/>
 					<Heading color="gray.300">Overview</Heading>
-				</Stack>
-			</HStack>
+				</Stack> */}
+				</HStack>
+			</Center>
 			{/* <HStack
 				w="full"
 				spacing={2}
