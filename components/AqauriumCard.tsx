d5244f59675ec58f456dcd332f5f41d988b52762
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-02 23:52:21 -0500

    refactor: updating homepage tanks look

--- components/AqauriumCard.tsx
+++ components/AqauriumCard.tsx
@@ -4,7 +4,8 @@ import handleViewport from 'react-in-viewport';
 import { Tank } from '@prisma/client';
 import { Card } from '@components/Card';
 import { trpc } from '@utils/trpc';
-import { Box, Center, Grid, GridItem, Text } from '@chakra-ui/react';
+import { BsArrowRight } from 'react-icons/bs';
+import { Box, Center, HStack, Stack, Text } from '@chakra-ui/react';
 
 export interface CardProps {
 	text?: string;
@@ -34,31 +35,37 @@ function AquariumCard(props: {
 			subHeading={props.subHeading}
 		>
 			{data ? (
-				<Grid templateColumns="repeat(2,1fr)" gap={3}>
+				<Stack w="full">
 					{data.tanks.map(({ id, name }: Tank) => (
-						<GridItem key={id} width="full" height="150px">
-							<Link href={`/aquarium/${id}`} passHref>
-								<Box
-									boxSize="full"
-									border="1px solid white"
-									borderRadius="15px"
-									bgSize="cover"
-									scrollSnapAlign="start"
-									bgGradient={`linear-gradient(135deg, rgba(250,250,250,1) 0%, rgba(250,250,250,0) 50%), url('https://images.unsplash.com/photo-1599492816933-2101fe60bc72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`}
-								/>
-							</Link>
-							<Text
-								color="white"
-								fontSize="24px"
-								fontWeight="semibold"
-								textAlign="center"
-								isTruncated
+						<Link href={`/aquarium/${id}`} passHref>
+							<HStack
+								w="full"
+								justify="center"
+								spacing="10"
+								shouldWrapChildren
 							>
-								{name}
-							</Text>
-						</GridItem>
+								<Box>
+									<Text
+										color="white"
+										fontSize="24px"
+										fontWeight="semibold"
+										textAlign="center"
+										isTruncated
+									>
+										{name}
+									</Text>
+								</Box>
+								<Box boxSize="full" rounded="full">
+									<BsArrowRight
+										color="white"
+										width={30}
+										height={30}
+									/>
+								</Box>
+							</HStack>
+						</Link>
 					))}
-				</Grid>
+				</Stack>
 			) : (
 				<Center>
 					<Spinner />
