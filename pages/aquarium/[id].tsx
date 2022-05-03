2a392b9898223c4190b444070360e0714b5b8944
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-02 21:35:04 -0500

    feat: rough overview

--- pages/aquarium/[id].tsx
+++ pages/aquarium/[id].tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Spinner from '@components/Spinner';
 import { trpc } from '@utils/trpc';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { Pencil1Icon } from '@radix-ui/react-icons';
@@ -8,17 +9,25 @@ import {
 	Box,
 	BoxProps,
 	Center,
+	Grid,
+	GridItem,
 	Heading,
 	HStack,
 	Stack,
+	Stat,
+	StatArrow,
+	StatGroup,
+	StatHelpText,
+	StatLabel,
+	StatNumber,
 	Text,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { FaWater } from 'react-icons/fa';
 
 const MotionBox = motion<BoxProps>(Box);
 const TankOptions: { label: string }[] = [
 	{
-		label: 'Tank Health',
+		label: 'Overview',
 	},
 	{
 		label: 'Reminders',
@@ -39,10 +48,17 @@ export default function Aquarium() {
 	return (
 		<Box w="100vw" h="full">
 			{data ? (
-				<Stack align="center" spacing={6} shouldWrapChildren>
+				<Stack
+					align="center"
+					h="full"
+					w="full"
+					mt="6"
+					spacing={6}
+					shouldWrapChildren
+				>
 					<Box
 						w="378px"
-						h="274"
+						h="274px"
 						pos="relative"
 						overflow="hidden"
 						borderRadius="15px"
@@ -100,6 +116,120 @@ export default function Aquarium() {
 								) : null}
 							</Text>
 						))}
+					</HStack>
+					<HStack w="full" shouldWrapChildren>
+						<Stack textAlign="left" spacing={3} shouldWrapChildren>
+							<Grid templateColumns="repeat(3, 1fr)">
+								<GridItem>
+									<Stack
+										textAlign="center"
+										align="center"
+										boxSize="full"
+										rounded="12px"
+									>
+										<FaWater color="white" />
+										<Text color="white">Type</Text>
+										<Text color="gray.400">Freshwater</Text>
+									</Stack>
+								</GridItem>
+								<GridItem>
+									<Stack
+										boxSize="full"
+										rounded="12px "
+										textAlign="center"
+										align="center"
+									>
+										<FaWater color="white" />
+										<Text color="white">Type</Text>
+										<Text color="gray.400">Freshwater</Text>
+									</Stack>
+								</GridItem>
+								<GridItem>
+									<Stack
+										boxSize="full"
+										rounded="12px"
+										textAlign="center"
+										align="center"
+									>
+										<FaWater color="white" />
+										<Text color="white">Type</Text>
+										<Text color="gray.400">Freshwater</Text>
+									</Stack>
+								</GridItem>
+							</Grid>
+							<Text color="white" fontSize="18px">
+								Stats
+							</Text>
+							<Box
+								bg="rgba(255,255,255,0.4)"
+								rounded="15px"
+								w="calc(100vw - 3rem)"
+								mb="3"
+							>
+								<StatGroup flexDir="row" textAlign="center">
+									<Stat>
+										<StatLabel flexDir="row">pH</StatLabel>
+										<StatNumber>7.0</StatNumber>
+										<StatHelpText>
+											<StatArrow type="increase" />
+											0.0%
+										</StatHelpText>
+									</Stat>
+									<Stat>
+										<StatLabel>Nirate</StatLabel>
+										<StatNumber>50</StatNumber>
+										<StatHelpText>
+											<StatArrow type="decrease" />
+											20.4%
+										</StatHelpText>
+									</Stat>
+									<Stat>
+										<StatLabel>Hardness</StatLabel>
+										<StatNumber>150</StatNumber>
+										<StatHelpText>
+											<StatArrow type="increase" />
+											5.3%
+										</StatHelpText>
+									</Stat>
+								</StatGroup>
+							</Box>
+							<Text fontSize="18px" color="white">
+								Suggestions
+							</Text>
+							<Box
+								bg="rgba(255,255,255,0.4)"
+								rounded="15px"
+								w="calc(100vw - 3rem)"
+								mb="3"
+							>
+								<StatGroup flexDir="row" textAlign="center">
+									<Stat>
+										<StatLabel flexDir="row">pH</StatLabel>
+										<StatNumber>7.0</StatNumber>
+										<StatHelpText>
+											<StatArrow type="increase" />
+											0.0%
+										</StatHelpText>
+									</Stat>
+									<Stat>
+										<StatLabel>Nirate</StatLabel>
+										<StatNumber>50</StatNumber>
+										<StatHelpText>
+											<StatArrow type="decrease" />
+											20.4%
+										</StatHelpText>
+									</Stat>
+									<Stat>
+										<StatLabel>Hardness</StatLabel>
+										<StatNumber>150</StatNumber>
+										<StatHelpText>
+											<StatArrow type="increase" />
+											5.3%
+										</StatHelpText>
+									</Stat>
+								</StatGroup>
+							</Box>
+						</Stack>
 					</HStack>
 				</Stack>
 			) : (
