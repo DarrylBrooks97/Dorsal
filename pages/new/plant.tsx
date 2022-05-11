6dc37531c3a00f2a7aebed55b5431dcb88f66bd6
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-10 19:02:09 -0500

    feat: more small updates

--- pages/new/plant.tsx
+++ pages/new/plant.tsx
@@ -1,6 +1,17 @@
-import { Button, HStack, Input, Stack } from '@chakra-ui/react';
-import { trpc } from '@utils/trpc';
+import Image from 'next/image';
 import { useState } from 'react';
+import { trpc } from '@utils/trpc';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import {
+	Box,
+	Button,
+	Center,
+	Heading,
+	HStack,
+	Input,
+	Stack,
+	Text,
+} from '@chakra-ui/react';
 
 export default function AddPlant() {
 	const { data } = trpc.useQuery(['user.tanks']);
@@ -8,7 +19,14 @@ export default function AddPlant() {
 
 	return (
 		<Stack w="full" h="100vh" p="6">
-			<HStack pos="sticky" top={0} zIndex="99" w="full" p="0.75rem 0rem">
+			<HStack
+				bg="rgba(0,0,0,0.8)"
+				pos="sticky"
+				top={0}
+				zIndex="99"
+				w="full"
+				p="0.75rem 0rem"
+			>
 				<Input
 					placeholder="Search Plants"
 					color="white"
@@ -27,7 +45,45 @@ export default function AddPlant() {
 						bg="rgba(238,235,211,0.4)"
 						w="calc(100vw - 3rem)"
 						h="100px"
-					></HStack>
+						pos="relative"
+					>
+						<Image
+							width="100%"
+							height="100%"
+							layout="fixed"
+							src="https://images.unsplash.com/photo-1567331711402-509c12c41959?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80"
+						/>
+						<Stack
+							h="full"
+							w="calc(100vw - 3rem - 30%)"
+							shouldWrapChildren
+						>
+							<Heading
+								fontSize="24px"
+								color="white"
+								textAlign="center"
+							>
+								Bamboo
+							</Heading>
+							<Text color="gray.300" fontSize="sm">
+								Type: Semi Aquatic
+							</Text>
+							<Text color="gray.300" fontSize="sm">
+								Species: Poales
+							</Text>
+						</Stack>
+						<Center
+							as="button"
+							pos="absolute"
+							boxSize="30px"
+							right="10px"
+							bottom="50%"
+							transform="translateY(50%)"
+							color="white"
+						>
+							<MdKeyboardArrowRight color="white" />
+						</Center>
+					</HStack>
 				))}
 			</Stack>
 		</Stack>
