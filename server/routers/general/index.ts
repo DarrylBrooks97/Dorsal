c138ee0cddabebdec6971c1a10947233dd6808da
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-13 20:06:57 -0500

    chore: updating general endpoint

--- server/routers/general/index.ts
+++ server/routers/general/index.ts
@@ -4,25 +4,22 @@ import { createRouter } from '../../createRouter';
 export const generalRouter = createRouter()
 	.query('fish', {
 		async resolve() {
-			const fish = await prisma.fish.findMany();
 			return {
-				fish,
+				fish: await prisma.fish.findMany(),
 			};
 		},
 	})
 	.query('tank', {
 		async resolve() {
-			const tank = await prisma.tank.findMany();
 			return {
-				tank,
+				tank: await prisma.tank.findMany(),
 			};
 		},
 	})
 	.query('plants', {
 		async resolve() {
-			const plants = await prisma.plant.findMany();
 			return {
-				plants,
+				plants: await prisma.plant.findMany(),
 			};
 		},
 	});
