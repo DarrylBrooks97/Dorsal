81cc74bd2c45c96181e0795a7c24588ac41572ec
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-22 12:45:44 -0500

    chore: adding domain to config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['images.unsplash.com', 'cdn.shopify.com'],
+		domains: ['images.unsplash.com', 'cdn.shopify.com', 's7d2.scene7.com'],
 	},
 };
 
