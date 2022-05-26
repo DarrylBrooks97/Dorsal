d6ff2a2e5a98e092cb90d2af7740413964ee523a
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-26 18:54:26 -0500

    chore: restructing files

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { inferQueryResponse } from 'pages/api/trpc/[trpc]';
+
 export const deepArrayFilter = <T>(obj: T[]): T[] => {
 	return [
 		...new Map(
@@ -7,4 +9,41 @@ export const deepArrayFilter = <T>(obj: T[]): T[] => {
 			})
 		).values(),
 	];
+};
+
+export type FetchedTankData = inferQueryResponse<'user.tanks.byId'>;
+
+export const getReminders = (fetchedData: FetchedTankData | undefined) => {
+	const fishFeeding = fetchedData?.fish.map((fish) => {
+		return {
+			id: fish.id,
+			fish_id: fish.fish_id,
+			name: fish.name,
+			image_url: fish.image_url,
+			maintained_at: fish.maintained_at as Date,
+		};
+	});
+
+	let todayReminders = fishFeeding?.filter((fish) => {
+		return new Date(fish.maintained_at) === new Date();
+	});
+
+	todayReminders = todayReminders?.sort((a: any, b: any) => {
+		return a.maintained_at - b.maintained_at;
+	});
+
+	let upcomingReminders = fishFeeding?.filter((fish) => {
+		return new Date(fish.maintained_at) > new Date();
+	});
+
+	upcomingReminders = upcomingReminders?.sort((a: any, b: any) => {
+		return a.maintained_at - b.maintained_at;
+	});
+
+	return {
+		reminders: {
+			today: todayReminders,
+			upcoming: upcomingReminders,
+		},
+	};
 };
