b1286485f5a31105984a0473842fd43859000a38
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-28 11:54:11 -0500

    refactor: homepage redo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import Link from 'next/link';
-import { Box, Center, Heading, Stack, Text } from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	Center,
+	Heading,
+	HStack,
+	Input,
+	Stack,
+	Text,
+} from '@chakra-ui/react';
 import { GetServerSidePropsContext } from 'next';
 import { getSession } from 'next-auth/react';
+import {
+	GitHubLogoIcon,
+	InstagramLogoIcon,
+	TwitterLogoIcon,
+} from '@radix-ui/react-icons';
 
 export const getServerSideProps = async ({
 	req,
@@ -18,38 +32,66 @@ export const getServerSideProps = async ({
 
 	return { props: { status: 'not logged in' } };
 };
-
+const JoinInput = () => {
+	return (
+		<HStack
+			pos="relative"
+			bg="rgba(217,217,217,0.5)"
+			backdropFilter={'blur(10px)'}
+			rounded="26px"
+			h="50px"
+			p="2"
+			w="full"
+		>
+			<Input
+				placeholder="Email"
+				type="email"
+				color="gray.200"
+				variant="unstyled"
+				rounded="26px"
+			/>
+			<Button pos="absolute" right="5px" rounded="26px">
+				Join
+			</Button>
+		</HStack>
+	);
+};
 export default function Home() {
 	return (
-		<Box w="100vw" h="100vh" bg="black" p="6">
+		<Center w="100vw" h="100vh" overflow="hidden" p="3">
 			<Stack
-				w="full"
-				pt={{ base: '80px', md: '20px' }}
+				w="calc(100vw - 3rem)"
+				alignItems="center"
 				textAlign="center"
 			>
-				<Heading fontSize="72px" fontWeight="medium" color="white">
+				<Heading color="white" fontSize="72px">
 					Dorsal
 				</Heading>
-				<Text fontSize="24px" fontWeight="bold" color="white">
-					Owning fish is complex
-				</Text>
-				<Text fontSize="16px" fontWeight="medium" color="white">
-					Learn from a community of experts at a click
-				</Text>
-			</Stack>
-			<Center mt="10">
-				<Link href="/login" passHref>
-					<Center
-						bgColor="green"
+				<Stack h="88px" w="294px" spacing={2}>
+					<Text color="white" fontWeight="600" fontSize="24px">
+						Aquariums are complex
+					</Text>
+					<Text color="gray.400" fontWeight="500">
+						Mollit magna sint incididunt exercitation sint do anim
+						magna{' '}
+					</Text>
+				</Stack>
+				<JoinInput />
+				<HStack>
+					<TwitterLogoIcon
 						color="white"
-						width="141px"
-						height="50px"
-						borderRadius="13px"
-					>
-						<Text fontSize="16px">Get Started</Text>
-					</Center>
-				</Link>
-			</Center>
-		</Box>
+						style={{ width: '30px', height: '30px' }}
+					/>
+					<GitHubLogoIcon
+						color="white"
+						style={{ width: '30px', height: '30px' }}
+					/>
+					<InstagramLogoIcon
+						color="white"
+						style={{ width: '30px', height: '30px' }}
+					/>
+				</HStack>
+			</Stack>
+		</Center>
 	);
 }
