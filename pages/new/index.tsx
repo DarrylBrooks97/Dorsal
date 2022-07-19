9dfe8fbf9d99641d3229a0ea3eb5283920e7a138
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-19 18:29:04 -0500

    feat: updating styling for new page

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,22 +1,89 @@
 import { Stack, VStack, Text, Heading, Center } from '@chakra-ui/react';
 import Link from 'next/link';
+import { FaFish } from 'react-icons/fa';
 import { GiChemicalTank } from 'react-icons/gi';
+import { RiPlantFill } from 'react-icons/ri';
 
 export default function New(): JSX.Element {
 	return (
 		<Center w="100vw" p="3">
-			<Stack spacing={8} flexDirection="column" w="full">
+			<Stack spacing={8} flexDirection="column" w="80%">
 				<Link href="/new/tank">
-					<VStack spacing={8} p="10px" rounded="150px" overflow="hidden">
-						<Stack w="full" bgColor="blue.400" justifyContent="center" alignItems="center">
-							<GiChemicalTank />
-							<Stack bgColor="gray.800" w="full" textAlign="center">
-								<Heading color="white">Tank</Heading>
-							</Stack>
+					<VStack spacing={4} minH="200px" bgColor="gray.800" rounded="15px" overflow="hidden">
+						<Stack
+							h="100px"
+							w="full"
+							bgColor="purple.600"
+							justifyContent="center"
+							alignItems="center"
+						>
+							<GiChemicalTank
+								fill="white"
+								style={{
+									width: '40px',
+									height: '40px',
+								}}
+							/>
 						</Stack>
+						<VStack justifyContent="center">
+							<Heading color="white" fontSize="3xl">
+								Tank
+							</Heading>
+							<Text color="gray.300">Add a new Aquarium</Text>
+						</VStack>
 					</VStack>
 				</Link>
 				<Link href="/new/fish">
+					<VStack spacing={4} minH="200px" bgColor="gray.800" rounded="15px" overflow="hidden">
+						<Stack
+							h="100px"
+							w="full"
+							bgColor="blue.400"
+							justifyContent="center"
+							alignItems="center"
+						>
+							<FaFish
+								fill="white"
+								style={{
+									width: '40px',
+									height: '40px',
+								}}
+							/>
+						</Stack>
+						<VStack justifyContent="center">
+							<Heading color="gray.300" fontSize="3xl">
+								Fish
+							</Heading>
+							<Text color="white">Add a new Fish to your Aquarium</Text>
+						</VStack>
+					</VStack>
+				</Link>
+				<Link href="/new/plant">
+					<VStack spacing={4} minH="200px" bgColor="gray.800" rounded="15px" overflow="hidden">
+						<Stack
+							h="100px"
+							w="full"
+							bgColor="green.600"
+							justifyContent="center"
+							alignItems="center"
+						>
+							<RiPlantFill
+								fill="white"
+								style={{
+									width: '40px',
+									height: '40px',
+								}}
+							/>
+						</Stack>
+						<VStack justifyContent="center">
+							<Heading color="white" fontSize="3xl">
+								Plant
+							</Heading>
+							<Text color="gray.300">Add a new Plant to your Aquarium</Text>
+						</VStack>
+					</VStack>
+				</Link>
+				{/* <Link href="/new/fish">
 					<VStack spacing={8}>
 						<Heading color="white">Fish</Heading>
 					</VStack>
@@ -25,7 +92,7 @@ export default function New(): JSX.Element {
 					<VStack spacing={8}>
 						<Heading color="white">Plant</Heading>
 					</VStack>
-				</Link>
+				</Link> */}
 			</Stack>
 		</Center>
 	);
