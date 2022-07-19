0423c362d6ae296c355f92b8ccc55b6d8ee387a9
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-18 23:12:36 -0500

    chore: paint job

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,13 +1,19 @@
-import { Box, Stack, VStack, Text, Heading, Center } from '@chakra-ui/react';
+import { Stack, VStack, Text, Heading, Center } from '@chakra-ui/react';
 import Link from 'next/link';
+import { GiChemicalTank } from 'react-icons/gi';
 
 export default function New(): JSX.Element {
 	return (
-		<Box w="full" h="full">
-			<Stack spacing={8} flexDirection="column">
+		<Center w="100vw" p="3">
+			<Stack spacing={8} flexDirection="column" w="full">
 				<Link href="/new/tank">
-					<VStack spacing={8}>
-						<Heading color="white">Tank</Heading>
+					<VStack spacing={8} p="10px" rounded="150px" overflow="hidden">
+						<Stack w="full" bgColor="blue.400" justifyContent="center" alignItems="center">
+							<GiChemicalTank />
+							<Stack bgColor="gray.800" w="full" textAlign="center">
+								<Heading color="white">Tank</Heading>
+							</Stack>
+						</Stack>
 					</VStack>
 				</Link>
 				<Link href="/new/fish">
@@ -21,6 +27,6 @@ export default function New(): JSX.Element {
 					</VStack>
 				</Link>
 			</Stack>
-		</Box>
+		</Center>
 	);
 }
